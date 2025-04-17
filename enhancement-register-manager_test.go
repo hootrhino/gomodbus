e1d09cc6d8c425fff03662c165053792f5efb5d7
wwhai <cnwwhai@gmail.com>
2025-04-17 20:11:27 +0800

    test: fix nil pointer

--- enhancement-register-manager_test.go
+++ enhancement-register-manager_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func Test_RegisterManager_Decode_bool(t *testing.T) {
+func MakeNewTestClient() Client {
 	handler := NewRTUClientHandler("COM3")
 	handler.BaudRate = 9600
 	handler.DataBits = 8
@@ -18,10 +18,14 @@ func Test_RegisterManager_Decode_bool(t *testing.T) {
 
 	err := handler.Connect()
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
 	defer handler.Close()
 	client := NewClient(handler)
+	return client
+}
+func Test_RegisterManager_Decode_bool(t *testing.T) {
+	client := MakeNewTestClient()
 	defer client.Close()
 	manager := NewRegisterManager(client, 10)
 
@@ -38,9 +42,6 @@ func Test_RegisterManager_Decode_bool(t *testing.T) {
 			BitPosition:  uint16(i),
 		})
 	}
-	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
-		t.Fatal(errLoad)
-	}
 	manager.SetOnErrorCallback(func(err error) {
 		t.Log(err)
 	})
@@ -53,6 +54,10 @@ func Test_RegisterManager_Decode_bool(t *testing.T) {
 			t.Log(fmt.Sprintf("tag:%s, value:%v", register.Tag, value.AsType))
 		}
 	})
+	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
+		t.Fatal(errLoad)
+	}
+
 	manager.Start()
 	for range 1 {
 		manager.ReadGroupedData()
@@ -61,28 +66,15 @@ func Test_RegisterManager_Decode_bool(t *testing.T) {
 	manager.Stop()
 }
 func Test_RegisterManager_Decode_uint16(t *testing.T) {
-	handler := NewRTUClientHandler("COM3")
-	handler.BaudRate = 9600
-	handler.DataBits = 8
-	handler.Parity = "N"
-	handler.StopBits = 1
-	handler.SlaveId = 1
-	handler.Logger = NewSimpleLogger(os.Stdout, LevelDebug)
-
-	err := handler.Connect()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer handler.Close()
-	client := NewClient(handler)
+	client := MakeNewTestClient()
 	defer client.Close()
 	manager := NewRegisterManager(client, 10)
 
 	registers := []DeviceRegister{}
 
 	registers = append(registers, DeviceRegister{
-		Tag:          fmt.Sprintf("tag:uint16:44031:%d", 1),
-		Alias:        fmt.Sprintf("tag:uint16:44031:%d", 1),
+		Tag:          fmt.Sprintf("tag:uint16-1:44031:%d", 1),
+		Alias:        fmt.Sprintf("tag:uint16-2:44031:%d", 1),
 		SlaverId:     uint8(1),
 		Function:     3,
 		ReadAddress:  0,
@@ -90,10 +82,16 @@ func Test_RegisterManager_Decode_uint16(t *testing.T) {
 		DataType:     "uint16", // ABFF = 10101011 11111111 = 44031
 		DataOrder:    "AB",
 	})
-
-	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
-		t.Fatal(errLoad)
-	}
+	registers = append(registers, DeviceRegister{
+		Tag:          fmt.Sprintf("tag:uint16-2:44031:%d", 1),
+		Alias:        fmt.Sprintf("tag:uint16-2:44031:%d", 1),
+		SlaverId:     uint8(1),
+		Function:     3,
+		ReadAddress:  0,
+		ReadQuantity: 1,
+		DataType:     "uint16", // ABFF = 10101011 11111111 = 44031
+		DataOrder:    "AB",
+	})
 	manager.SetOnErrorCallback(func(err error) {
 		t.Log(err)
 	})
@@ -106,6 +104,10 @@ func Test_RegisterManager_Decode_uint16(t *testing.T) {
 			t.Log(fmt.Sprintf("tag:%s, value:%v", register.Tag, value.AsType))
 		}
 	})
+	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
+		t.Fatal(errLoad)
+	}
+
 	manager.Start()
 	for range 1 {
 		manager.ReadGroupedData()
@@ -115,28 +117,14 @@ func Test_RegisterManager_Decode_uint16(t *testing.T) {
 }
 
 func Test_RegisterManager_Decode_uint32(t *testing.T) {
-	handler := NewRTUClientHandler("COM3")
-	handler.BaudRate = 9600
-	handler.DataBits = 8
-	handler.Parity = "N"
-	handler.StopBits = 1
-	handler.SlaveId = 1
-	handler.Logger = NewSimpleLogger(os.Stdout, LevelDebug)
-
-	err := handler.Connect()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer handler.Close()
-	client := NewClient(handler)
+	client := MakeNewTestClient()
 	defer client.Close()
 	manager := NewRegisterManager(client, 10)
-
 	registers := []DeviceRegister{}
 
 	registers = append(registers, DeviceRegister{
-		Tag:          fmt.Sprintf("tag:uint32:1078530010:%d", 1),
-		Alias:        fmt.Sprintf("tag:uint32:1078530010:%d", 1),
+		Tag:          fmt.Sprintf("tag:uint32-1:1078530010:%d", 1),
+		Alias:        fmt.Sprintf("tag:uint32-1:1078530010:%d", 1),
 		SlaverId:     uint8(1),
 		Function:     3,
 		ReadAddress:  0,
@@ -144,10 +132,16 @@ func Test_RegisterManager_Decode_uint32(t *testing.T) {
 		DataType:     "uint32", // 40 49 0f da = 01000000010010010000111111011010 = 1078530010
 		DataOrder:    "ABCD",
 	})
-
-	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
-		t.Fatal(errLoad)
-	}
+	registers = append(registers, DeviceRegister{
+		Tag:          fmt.Sprintf("tag:uint32-2:1078530010:%d", 1),
+		Alias:        fmt.Sprintf("tag:uint32-2:1078530010:%d", 1),
+		SlaverId:     uint8(1),
+		Function:     3,
+		ReadAddress:  0,
+		ReadQuantity: 2,
+		DataType:     "uint32", // 40 49 0f da = 01000000010010010000111111011010 = 1078530010
+		DataOrder:    "ABCD",
+	})
 	manager.SetOnErrorCallback(func(err error) {
 		t.Log(err)
 	})
@@ -160,6 +154,10 @@ func Test_RegisterManager_Decode_uint32(t *testing.T) {
 			t.Log(fmt.Sprintf("tag:%s, value:%v", register.Tag, value.AsType))
 		}
 	})
+	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
+		t.Fatal(errLoad)
+	}
+
 	manager.Start()
 	for range 1 {
 		manager.ReadGroupedData()
@@ -169,28 +167,13 @@ func Test_RegisterManager_Decode_uint32(t *testing.T) {
 }
 
 func Test_RegisterManager_Decode_float32(t *testing.T) {
-	handler := NewRTUClientHandler("COM3")
-	handler.BaudRate = 9600
-	handler.DataBits = 8
-	handler.Parity = "N"
-	handler.StopBits = 1
-	handler.SlaveId = 1
-	handler.Logger = NewSimpleLogger(os.Stdout, LevelDebug)
-
-	err := handler.Connect()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer handler.Close()
-	client := NewClient(handler)
+	client := MakeNewTestClient()
 	defer client.Close()
 	manager := NewRegisterManager(client, 10)
-
 	registers := []DeviceRegister{}
-
 	registers = append(registers, DeviceRegister{
-		Tag:          fmt.Sprintf("tag:float3232:3.1415926:%d", 1),
-		Alias:        fmt.Sprintf("tag:float3232:3.1415926:%d", 1),
+		Tag:          fmt.Sprintf("tag:float3232-1:3.1415926:%d", 1),
+		Alias:        fmt.Sprintf("tag:float3232-1:3.1415926:%d", 1),
 		SlaverId:     uint8(1),
 		Function:     3,
 		ReadAddress:  0,
@@ -198,10 +181,16 @@ func Test_RegisterManager_Decode_float32(t *testing.T) {
 		DataType:     "float32", // 40 49 0f da = 01000000010010010000111111011010 = 3.1415926
 		DataOrder:    "ABCD",
 	})
-
-	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
-		t.Fatal(errLoad)
-	}
+	registers = append(registers, DeviceRegister{
+		Tag:          fmt.Sprintf("tag:float3232-2:3.1415926:%d", 1),
+		Alias:        fmt.Sprintf("tag:float3232-2:3.1415926:%d", 1),
+		SlaverId:     uint8(1),
+		Function:     3,
+		ReadAddress:  0,
+		ReadQuantity: 2,
+		DataType:     "float32", // 40 49 0f da = 01000000010010010000111111011010 = 3.1415926
+		DataOrder:    "ABCD",
+	})
 	manager.SetOnErrorCallback(func(err error) {
 		t.Log(err)
 	})
@@ -214,6 +203,9 @@ func Test_RegisterManager_Decode_float32(t *testing.T) {
 			t.Log(fmt.Sprintf("tag:%s, value:%v", register.Tag, value.AsType))
 		}
 	})
+	if errLoad := manager.LoadRegisters(registers); errLoad != nil {
+		t.Fatal(errLoad)
+	}
 	manager.Start()
 	for range 1 {
 		manager.ReadGroupedData()
